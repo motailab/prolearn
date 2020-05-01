8660687717aa6ea81197cee8d66f6776f8485288
motailab <motailab@gmail.com>
2020-05-01 10:38:07 +0600

    Updates

---
+++ precache-manifest.0c64b44105cd607c763ab3af0a47461c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e753deb7b05085f4ac239871b316911",
+    "url": "/prolearn/index.html"
+  },
+  {
+    "revision": "00b1d467d1c6e727a94d",
+    "url": "/prolearn/static/css/main.259bbef9.chunk.css"
+  },
+  {
+    "revision": "97ecd25e5436fb932728",
+    "url": "/prolearn/static/js/2.c7f85c71.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/prolearn/static/js/2.c7f85c71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00b1d467d1c6e727a94d",
+    "url": "/prolearn/static/js/main.11fbf57f.chunk.js"
+  },
+  {
+    "revision": "3356fd513be4a85554a8",
+    "url": "/prolearn/static/js/runtime-main.afea68a5.js"
+  },
+  {
+    "revision": "dab5baf00c7ee39da3e44d3e73909462",
+    "url": "/prolearn/static/media/diagram.dab5baf0.svg"
+  },
+  {
+    "revision": "9bff32ab43d5ae91cb2f2e48198204f5",
+    "url": "/prolearn/static/media/logo.9bff32ab.svg"
+  }
+]);
