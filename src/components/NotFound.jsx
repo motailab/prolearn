a38d1ac7a814621dfbb558bc5e85f761aa77bcd7
motailab <motailab@gmail.com>
2020-05-06 14:54:52 +0600

    Updates

--- src/components/NotFound.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function NotFound(params) {
-    return (
-        <div className="alert alert-warning text-center p-4">
-            <h5>Component Not Found Try</h5>
-        </div>
-    )
-};
--- src/utils/dataClient.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-axios.defaults.baseURL = 'https://prolearn-fb6bb.firebaseio.com';
-
-export const getSubjects = () => {
-    return axios.get('/subjects.json').then(resp => resp.data);
-}
