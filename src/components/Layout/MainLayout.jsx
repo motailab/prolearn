e7c49807cf11477568bd620ba3eed2ac23ebcf12
motailab <motailab@gmail.com>
2020-05-06 15:24:08 +0600

    removed header.

--- src/components/Layout/MainLayout.jsx
+++ src/components/Layout/MainLayout.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import SideBar from '../SideBar'
-import Header from '../Header';
 import FullScreenConfetti from '../FullScreenConfetti/FullScreenConfetti'
 
 export default function MainLayout({children}) {
